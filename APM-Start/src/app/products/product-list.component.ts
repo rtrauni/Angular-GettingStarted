7434d885d2e69808ff5428257d32d1dbbd0f5d17
Rudolf Traunmüller <rudolf.traunmueller@maat.at>
2020-01-02 14:20:32 +0100

    Data Binding & Pipes

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
     products: any[] = [
         {
             "productId": 1,
@@ -58,4 +62,8 @@ export class ProductListComponent {
             "imageUrl": "assets/images/xbox-controller.png"
         }
     ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    };
 }

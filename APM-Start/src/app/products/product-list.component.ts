1e02ea346dfb71b9ded1b5a3f9a7ea28b6ef4ace
Rudolf Traunmüller <rudolf.traunmueller@maat.at>
2020-01-03 10:58:36 +0100

    Building Nested Components

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -94,4 +94,9 @@ export class ProductListComponent implements OnInit {
     ngOnInit(): void {
         console.log('In OnInit');
     }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+        console.log(message);
+    }
 }
